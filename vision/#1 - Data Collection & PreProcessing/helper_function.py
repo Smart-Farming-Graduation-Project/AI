14931eb48e8507053db496be8a861584ce98d733
Marwan <marwan-walid1@hotmail.com>
2025-01-29 21:10:51 +0200

    added save image and annotation function

--- vision/#1 - Data Collection & PreProcessing/helper_function.py
+++ vision/#1 - Data Collection & PreProcessing/helper_function.py
@@ -1,11 +1,12 @@
 import glob
+import pandas as pd
 import shutil
 import os
 from xml.etree import ElementTree as et
 
 
 # Function To Create Train, Test, Validation Folders
-def CreateHierarchicalFolders(ParentFile = 'Data'):
+def CreateHierarchicalFolders(ParentFile='Data'):
     '''
         This Function Used To Create Folders To Contain Data In Structure 
         (
@@ -14,7 +15,7 @@ def CreateHierarchicalFolders(ParentFile = 'Data'):
             test  -> images, label
         )
     '''
-    if not os.path.exists(ParentFile) :
+    if not os.path.exists(ParentFile):
         os.makedirs(ParentFile + '/train' + '/images')
         os.makedirs(ParentFile + '/train' + '/label')
         os.makedirs(ParentFile + '/val' + '/images')
@@ -23,40 +24,43 @@ def CreateHierarchicalFolders(ParentFile = 'Data'):
         os.makedirs(ParentFile + '/test' + '/label')
         print("All File Created 👌")
         return True
-    else :
+    else:
         print("The Files Already Existed !")
         return False
 
 
 # Function To Get Label From XML File
 def ExtractLabel(filepath):
-        '''
-            Function To EXtract Annotation From File (XML)
-            Parameters -> XML File Path
-            Return -> List Contain All Objects In Image
-        '''
-        # Get Root Of Hierarchical 
-        root = et.parse(filepath).getroot()
+    '''
+        Function To EXtract Annotation From File (XML)
+        Parameters -> XML File Path
+        Return -> List Contain All Objects In Image
+    '''
+    # Get Root Of Hierarchical
+    root = et.parse(filepath).getroot()
 
-        # Get Name Image, Width, Height
-        image_name = root.find('filename').text
-        width = root.find('size').find('width').text
-        height = root.find('size').find('height').text
+    # Get Name Image, Width, Height
+    image_name = root.find('filename').text
+    width = root.find('size').find('width').text
+    height = root.find('size').find('height').text
 
-        # Get Objects, Bounding Box
-        objs = root.findall('object')
-        parser = []
-        for obj in objs:
-            name = obj.find('name').text
-            bndbox = obj.find('bndbox')
-            xmin = bndbox.find('xmin').text
-            xmax = bndbox.find('xmax').text
-            ymin = bndbox.find('ymin').text
-            ymax = bndbox.find('ymax').text
-            parser.append([image_name, width, height, name, xmin, xmax, ymin, ymax])
-        return parser
+    # Get Objects, Bounding Box
+    objs = root.findall('object')
+    parser = []
+    for obj in objs:
+        name = obj.find('name').text
+        bndbox = obj.find('bndbox')
+        xmin = bndbox.find('xmin').text
+        xmax = bndbox.find('xmax').text
+        ymin = bndbox.find('ymin').text
+        ymax = bndbox.find('ymax').text
+        parser.append([image_name, width, height,
+                      name, xmin, xmax, ymin, ymax])
+    return parser
 
 # Convert Function Maps the Five numbers of Cordinates to four Numbers for bounding box
+
+
 def Convert_coordinates(width, height, xmin, xmax, ymin, ymax):
     '''
     Functionality of this function Converts bounding box coordinates (xmin, xmax, ymin, ymax)
@@ -69,9 +73,11 @@ def Convert_coordinates(width, height, xmin, xmax, ymin, ymax):
     x_center = (xmin / width) + (new_width / 2)
     y_center = (ymin / height) + (new_height / 2)
 
-    return x_center , y_center , new_width , new_height
+    return x_center, y_center, new_width, new_height
 
 # Function To Get Paths For All Images And Labels In Folder
+
+
 def getPaths(pathfile):
     '''
         This Function To Collect Paths For All Images And Annotation (xml) file in folder
@@ -79,13 +85,13 @@ def getPaths(pathfile):
         parameters -> Take PathFile That Contain images plus annotation 
         return     -> Return List Of Images Paths And Labeld Paths
     '''
-     
+
     # this line to get the extension of images like (.jpg, .png, ...)
     img_ext = os.path.splitext(os.listdir(pathfile)[0])[1]
     images_paths = glob.glob(pathfile + '/*' + img_ext)
     label_paths = glob.glob(pathfile + '/*.xml')
     return images_paths, label_paths
-    
+
 
 # Function To Mapping Class Name To ID and vice-versa
 
@@ -93,44 +99,71 @@ Map_Class_To_Number = {}
 Map_Number_To_Class = {}
 counter = 1
 
+
 def map_class_to_number(class_name):
-    
+
     global counter, Map_Class_To_Number, Map_Number_To_Class
-    
+
     if class_name not in Map_Class_To_Number:
         Map_Class_To_Number[class_name] = counter
         Map_Number_To_Class[counter] = class_name
         counter += 1
     return Map_Class_To_Number[class_name]
 
+
 def map_number_to_class(number):
-    
+
     global Map_Number_To_Class
-    
+
     if number in Map_Number_To_Class:
         return Map_Number_To_Class[number]
     return "Class Not Found"
 
 
-print(map_class_to_number("A"))  
-print(map_class_to_number("B"))  
-print(map_class_to_number("C"))  
+print(map_class_to_number("A"))
+print(map_class_to_number("B"))
+print(map_class_to_number("C"))
 
 print("-" * 30)
 # if class name came as a list
-class_names = ["D", "E" , "F" , "G"]  
+class_names = ["D", "E", "F", "G"]
 for class_name in class_names:
     print(map_class_to_number(class_name))
 
 print("-" * 30)
 
-print(map_number_to_class(1))  
-print(map_number_to_class(2))  
-print(map_number_to_class(3))  
+print(map_number_to_class(1))
+print(map_number_to_class(2))
+print(map_number_to_class(3))
 print(map_number_to_class(4))
+
+
+
+def save_image_and_annotation(df:pd.DataFrame , dst_annotation_dir , dst_image_dir):
+    gr_by = df.groupby("path")
+    SEP = os.sep
+    for path, groups in gr_by:
+        label_path = dst_annotation_dir + SEP + path.split(SEP)[-1][0:-3] + 'txt'
+        content = ""
+        for i, row in groups.iterrows():
+            x_center, y_center, width, height = row['x_center'], row['y_center'], row['new_width'], row['new_height']
+            image_id = row["image_id"]
+            content += f"{image_id} {width} {height} {x_center} {y_center}"
+            if i != len(groups)-1:
+                content += "\n"
+        save_label(label_path, content)
+        shutil.copy(path, dst_image_dir + SEP + path.split(SEP)[-1])
+        print("Successfully added")
+
+
+
+
+
+def save_label(label_path , content):
+    with open(label_path,"w+") as file:
+        file.write(content)
 
 
 
 if __name__ == '__main__':
     pass
-    
