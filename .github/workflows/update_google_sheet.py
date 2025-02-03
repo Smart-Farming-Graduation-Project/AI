bde2d8ab89f19b5fc12221962d7949abc0d15d9e
Mohamed Elsayed <86891363+Mohamedzonkol@users.noreply.github.com>
2025-02-03 15:55:48 +0200

    Update update_google_sheet.py

--- .github/workflows/update_google_sheet.py
+++ .github/workflows/update_google_sheet.py
@@ -85,7 +85,7 @@ def update_google_sheet(ID, Task_Name, Assigned_Member, Assigned_Date, Date_Comp
             valueInputOption="RAW", body=body).execute()
         print(f"Task {ID} updated in row {task_row}.")
     else:
-        append_range = f"Web!A{len(values) + 1}:I{len(values) + 1}"  # Get the next available row
+        append_range = f"AI!A{len(values) + 1}:I{len(values) + 1}"  # Get the next available row
         body = {
             'values': [row_data]
         }
